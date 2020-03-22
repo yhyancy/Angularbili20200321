7674fd1bc792b3f7ac11ecc7a9a8cee39508fe9e
Hua Yang <dlyhua@cn.ibm.com>
2020-03-22 15:49:16 +0800

    ngIfnfSwitch事件管道

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -7,9 +7,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  //循环数组，显示索引号
+  public list: any[] = [{ 'title': '我是新闻1' }, { 'title': '我是新闻2' }, { 'title': '我是新闻3' }]
+  public flag: boolean = true
+  public orderStatus: number = 1 // 1.没提交订单 2. 提交订单 3. 已发货 4. 已收货
+  public sty: string = 'red'
+  public today: any = new Date()
+  public title: any = '我是title'
   constructor() { }
 
   ngOnInit() {
   }
+  run() {
+    alert('事件方法')
+  }
+  getData() {
+    alert(this.today)
+  }
+  setData() {
+    this.title = '我是改变后的title'
+  }
+  keyDown(e) {
+    console.log(e) //获取dom对象
+    console.log(e.target.value)
+  }
+  keyUp(e) {
+    console.log(e) //获取dom对象
+    console.log(e.target.value)
+  }
+  runEvent(e) {
+    var dom: any = e.target
+    dom.style.color = 'red'
 
+  }
 }
