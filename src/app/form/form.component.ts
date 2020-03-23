e3d0e2af1cb7725c79252b3085240cb1d57448b1
Hua Yang <dlyhua@cn.ibm.com>
2020-03-24 00:21:59 +0800

    form表单双向绑定

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -7,9 +7,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormComponent implements OnInit {
 
+  public peopleinfo: any =
+    {
+      username: '',
+      sex: '1',
+      cityList: ['北京', '上海', '深圳'],
+      city: '北京',
+      hobby: [{ title: '吃饭', checked: false },
+      { title: '睡觉', checked: false },
+      { title: '打豆豆', checked: false }
+      ],
+      note: ''
+    }
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  // jquery获取元素数据
+  // let unameDom: any = document.getElementById('username')
+  // console.log(unameDom.value)
+
+  // 双向数据绑定来获取元素数据
+  doSubmit() {
+
+    console.log(this.peopleinfo)
+
+  }
 }
