00f395cfab7db9bd9533679cc958cb06a82df7d3
Hua Yang <dlyhua@cn.ibm.com>
2020-03-28 19:37:10 +0800

    子组件->父组件传值

--- src/app/components/cfooter/cfooter.component.ts
+++ src/app/components/cfooter/cfooter.component.ts
@@ -6,10 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cfooter.component.scss']
 })
 export class CfooterComponent implements OnInit {
+  public msg: any="我是子组件的msg"
 
   constructor() { }
 
   ngOnInit() {
   }
-
+public run(){
+  alert('我是子组件的run方法')
+}
 }
--- src/app/components/fnews/fnews.component.ts
+++ src/app/components/fnews/fnews.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-fnews',
@@ -6,10 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fnews.component.scss']
 })
 export class FnewsComponent implements OnInit {
+  @ViewChild('footer',{static:true}) footer:any
 
   constructor() { }
 
   ngOnInit() {
   }
-
+  getChildMsg(){
+    alert(this.footer.msg)
+  }
+  getChildRun(){
+    alert(this.footer.run())
+  }
 }
